e4ed0f57425e49e1b734a9b3f73040ed17c24498
tuntuta123 <123tuntuta@gmail.com>
2026-01-21 15:32:16 +0100

    small bugfix quicksort

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -16,16 +16,18 @@ public class QuickSort{
 		}
 	}
 	
-	static void partition(List<Integer> list, int low, int high){
+	static int partition(List<Integer> list, int low, int high){
 		int pivot = list.get(high);
 		int i = low - 1;
 		
 		for (int j = low ; j <= high -1 ; j++){
 			if (list.get(j) < pivot){
 				i++;
-				swap(list,i,j)
+				swap(list,i,j);
 			}
 		}
+		swap(list, i + 1, high);
+        	return i + 1;
 	}
 	
 	static void swap(List<Integer> list, int first, int second) {
